129cef5b536912b9f37b0cf54ca67631ca5022d4
Ubuntu <ubuntu@web.vm>
2017-07-31 19:05:47 +0000

    Corrected copyright header

--- tests/StreamTest.php
+++ tests/StreamTest.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Idoheo Uri Query package.
- * (c) Repository contributors
+ * This file is part of Idoheo Stream package.
+ * (c) 2017 Repository contributors
  *
  * This source file is subject to the MIT license.
  * Copy of license is located with this source code in the file LICENSE.
